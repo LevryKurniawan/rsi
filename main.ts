4b8d7a3cae34a2fc68f28342b19573bfd1fca956
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 15:11:08 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,1 @@
-import { Cron } from "https://deno.land/x/deno_cron/cron.ts";
-
-new Cron("*/1 * * * *", () => fetch("https://ntfy.sh/A", { method: "POST", body: "1" }));
+Deno.cron("*/1 * * * *", () => fetch("https://ntfy.sh/A", { method: "POST", body: "1" }));
