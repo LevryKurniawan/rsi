09ed28765e4b64a6debe2225106e4ebc7fa86b20
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 15:15:46 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-Deno.cron("*/1 * * * *", () => fetch("https://ntfy.sh/A", { method: "POST", body: "1" }).then(() => new Response()));
+Deno.cron("*/1 * * * *", () => {
+  fetch("https://ntfy.sh/A", { method: "POST", body: "1" });
+  return new Response("Job executed");
+});
