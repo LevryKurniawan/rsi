ce7c80e928e6662028aeafd1872219522692bd94
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 16:59:05 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,5 @@ Deno.cron("RSI", "* * * * *", async () => {
   const url = "https://api.binance.com/api/v3/klines?symbol=BTCFDUSD&interval=1m&limit=7"
   const res = await fetch(url)
   const d = await res.json()
-  const c = d.map(i => +i[4])
-  const g = c.slice(1).map((v, i) => v - c[i])
-  const u = g.map(x => x > 0 ? x : 0).reduce((a, b) => a + b) / 6
-  const dn = g.map(x => x < 0 ? -x : 0).reduce((a, b) => a + b) / 6
-  const rsi = 100 - 100 / (1 + u / dn)
-  fetch("https://ntfy.sh/A", { method: "POST", body: rsi.toFixed(2) })
+  console.log(d) // Debug: lihat hasil response
 })
