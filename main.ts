da41f32d0feda4e8915db79d942a5bdd263f1bdb
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 16:19:02 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 Deno.cron("Send BTC price every minute", "*/1 * * * *", async () => {
   try {
-    const res = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");  // Simbol BTCUSDT
+    const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
     const data = await res.json();
 
-    if (data && data.price) {
-      console.log(`BTC/USDT Price: ${data.price}`);  // Debug log harga
-      await fetch("https://ntfy.sh/A", { method: "POST", body: data.price });
+    if (data && data.bitcoin && data.bitcoin.usd) {
+      console.log(`BTC/USDT Price: ${data.bitcoin.usd}`);
+      await fetch("https://ntfy.sh/A", { method: "POST", body: `${data.bitcoin.usd}` });
     } else {
       console.error("Failed to get price or unexpected response format", data);
     }
