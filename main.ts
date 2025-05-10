cb36e94bcf61a8a3c3f9524c3e3835693bf09125
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 16:34:24 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-Deno.cron("*/1 * * * *", () => fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=fdusd")
+Deno.cron("*/1 * * * *", () => fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCFDUSD")
   .then(r => r.json())
-  .then(d => fetch("https://ntfy.sh/A", { method: "POST", body: `${d.bitcoin.fdusd}` })));
+  .then(d => fetch("https://ntfy.sh/A", { method: "POST", body: `${d.price}` })));
