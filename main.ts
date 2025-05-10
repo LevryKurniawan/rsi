1ad5af011f11d5a9d9ad7003634c55ba166bd9b1
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 16:10:37 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-Deno.cron("Send BTC price every minute", "*/1 * * * *", () =>
-  fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCFDUSD")
-    .then(r => r.json())
-    .then(d => fetch("https://ntfy.sh/A", { method: "POST", body: d.price }))
-);
+Deno.cron("Send BTC price every minute", "*/1 * * * *", async () => {
+  const res = await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCFDUSD");
+  const data = await res.json();
+  
+  fetch("https://ntfy.sh/A", { method: "POST", body: data.price });
+  return new Response(data.price);  // Menampilkan harga di respons
+});
