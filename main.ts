dd907269c16b58623f2ab381448e85cbf868b317
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 15:24:34 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-Deno.cron("*/1 * * * *", () => {
-  fetch("https://ntfy.sh/A", { method: "POST", body: "1" });
-  return new Response("Cron job executed");
-});
-
-addEventListener("fetch", (event) => {
-  event.respondWith(new Response("OK"));
-});
+Deno.cron("Run once a minute", "* * * * *", () => fetch("https://ntfy.sh/A", { method: "POST", body: "1" }));
