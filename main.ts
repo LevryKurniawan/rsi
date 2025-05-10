36f0429bfb2657352efd319fa16411c74a791d3f
Levry Kurniawan <40631294+LevryKurniawan@users.noreply.github.com>
2025-05-10 15:55:25 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,1 @@
-Deno.cron("C","* * * * *",()=>fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCFDUSD").then(r=>r.json()).then(d=>fetch("https://ntfy.sh/A",{method:"POST",headers:{"Content-Type":"text/plain"},body:String(d.price)})))
+Deno.cron("C","* * * * *",()=>fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCFDUSD").then(r=>r.json()).then(d=>fetch("https://ntfy.sh/A",{method:"POST",headers:{"Content-Type":"text/plain"},body:d.price})))
